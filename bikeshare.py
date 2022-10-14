581b1089a1b6afaa18df5c3ed86bd5c87b2bfef0
Don Lockwood <dmlockwood36@gmail.com>
2022-10-14 12:11:37 -0700

    Added bold text in terminal for readability

--- bikeshare.py
+++ bikeshare.py
@@ -32,7 +32,8 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
 
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('\033[1m' + 'Hello! Let\'s explore some US bikeshare data!')
+    print('\033[0m')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         try:
